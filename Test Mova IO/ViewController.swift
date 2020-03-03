5882be11fe8201f4b58b71002e2109f61b575187
Artem <f159752@gmail.com>
2020-03-03 20:37:02 +0200

    Initial Commit

---
+++ Test Mova IO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Mova IO
+//
+//  Created by Artem Shpilka on 3/3/20.
+//  Copyright © 2020 Artem Shpilka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
